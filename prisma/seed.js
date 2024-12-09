69c50b316df682421b19e38d429b598765ef0ae5
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-12-09 00:31:50 -0500

    All data models updated except for recipes (still a little unsure on that one)

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,18 +3,31 @@ const prisma = new PrismaClient()
 
 
 const initialCookingMethods = [
-    { name: 'Sautee',   length: '5 minutes'},
-    { name: 'Toast',    length: '2 minutes'},
-    { name: 'Bake',     length: '30 minutes'},
-];
+    { id: 1,    name: 'sautee',   length: '5 minutes'},
+    { id: 2,    name: 'toast',    length: '2 minutes'},
+    { id: 3,    name: 'bake',     length: '30 minutes'},
+]
 
 const initialIngredients = [
-    { name: 'Butter',  amount: '4 tablespoons'},
-    { name: 'Flour',   amount: '2 cups'},
-    { name: 'Sugar',   amount: '1 cup'},
+    { id: 1,    name: 'butter',  amount: '4 tablespoons'},
+    { id: 2,    name: 'flour',   amount: '2 cups'},
+    { id: 3,    name: 'sugar',   amount: '1 cup'},
+]
+
+const initialPreparationMethods = [
+    { id: 1,    name: 'whip',   length: '5 minutes'},
+    { id: 2,    name: 'blend',    length: 'until fully incorporated'},
+    { id: 3,    name: 'chop',     length: 'until minced'},
+]
+
+const initialEquipment = [
+    { id: 1,    name: 'stand-mixer'},
+    { id: 2,    name: 'blender'},
+    { id: 3,    name: 'mixing bowl'},
 ]
 
 const seed = async () => {
+
     await prisma.cookingMethod.deleteMany();
     for (const cookingMethod of initialCookingMethods){
         await prisma.cookingMethod.createMany({
@@ -26,6 +39,20 @@ const seed = async () => {
     for (const ingredient of initialIngredients){
         await prisma.ingredient.createMany({
             data: ingredient,
+        });
+    }
+
+    await prisma.preparationMethod.deleteMany();
+    for (const preparationMethod of initialPreparationMethods){
+        await prisma.preparationMethod.createMany({
+            data: preparationMethod,
+        });
+    }
+
+    await prisma.equipment.deleteMany();
+    for (const equipment of initialEquipment){
+        await prisma.equipment.createMany({
+            data: equipment,
         });
     }
 };
