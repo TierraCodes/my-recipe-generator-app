63d06fe21edac374f40fed74252d7584f7d3801f
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-12-09 00:14:37 -0500

    Database up and running!!
    sample data for now and not all models are up, but it is definitely working!!

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,22 +1,31 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 
 const initialCookingMethods = [
-    { name: 'Sautee'},
-    { name: 'Deep Fry'},
-    { name: 'Bake'},
-    { name: 'Microwave'},
-    { name: 'Toast'},
-    { name: 'Boil'},
+    { name: 'Sautee',   length: '5 minutes'},
+    { name: 'Toast',    length: '2 minutes'},
+    { name: 'Bake',     length: '30 minutes'},
 ];
 
-const seed = async () => {
-    await prisma.cookingMethods.deleteMany();
+const initialIngredients = [
+    { name: 'Butter',  amount: '4 tablespoons'},
+    { name: 'Flour',   amount: '2 cups'},
+    { name: 'Sugar',   amount: '1 cup'},
+]
 
-    for (const cookingMethods of initialCookingMethods){
-        await prisma.cookingMethods.create({
-            data:cookingMethods,
+const seed = async () => {
+    await prisma.cookingMethod.deleteMany();
+    for (const cookingMethod of initialCookingMethods){
+        await prisma.cookingMethod.createMany({
+            data: cookingMethod,
+        });
+    }
+
+    await prisma.ingredient.deleteMany();
+    for (const ingredient of initialIngredients){
+        await prisma.ingredient.createMany({
+            data: ingredient,
         });
     }
 };
